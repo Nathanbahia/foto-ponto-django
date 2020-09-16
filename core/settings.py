0a6d62661b9f3632441dee791c9be399a80277ab
nathanbahia <nathanbabahia@gmail.com>
2020-09-16 09:59:19 -0300

    Altera debug e allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x((^9@ds$1^89cr(mpur#we97&^)4bpmointwa8js21qa)*yr_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

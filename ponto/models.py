0fae4eb2e4f6d33f8db6e56c77b2f5716e6b1ed9
nathanbahia <nathanbabahia@gmail.com>
2020-09-17 19:01:30 +0000

    Cria validação dos formulários usando jquery

--- ponto/models.py
+++ ponto/models.py
@@ -3,10 +3,10 @@ from stdimage.models import StdImageField
 
 class Ponto(models.Model):
     AVES = [
-        ('SIM', 'SIM'),
-        ('NÃO', 'NÃO')
+        ('SIM', 'Sim'),
+        ('NÃO', 'Não')
     ]
     data = models.CharField("Data", max_length=30)
     matricula = models.CharField("Matrícula", max_length=10)
     aves = models.CharField('Presença de aves no local?', choices=AVES, max_length=4)
-    foto = StdImageField("", upload_to='media')      
+    foto = StdImageField("", upload_to='media')

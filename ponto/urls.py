2cf6f0d85139578704ee815efa1cd9b74a618e9e
nathanbahia <nathanbabahia@gmail.com>
2020-09-18 10:17:50 -0300

    Cria sistema de envio de e-mails

--- ponto/urls.py
+++ ponto/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, historico
+from .views import index, historico, visualizar_foto
 
 
 urlpatterns = [
     path('', index, name='index'),
-    path('historico', historico, name='historico')
+    path('historico', historico, name='historico'),
+    path('visualizacao/<int:pk>', visualizar_foto, name='visualizar')
 ]
--- ponto/views.py
+++ ponto/views.py
@@ -1,10 +1,14 @@
-from datetime import datetime
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.contrib import messages
+from django.core.mail import EmailMultiAlternatives
+from core import settings
 from .forms import PontoForm
 from .models import Ponto
+from datetime import datetime
 
 
+@login_required
 def index(request):
     if str(request.method) == "POST":
         form = PontoForm(request.POST, request.FILES)
@@ -15,6 +19,30 @@ def index(request):
             foto = form.cleaned_data['foto']
             ponto = Ponto(data=data, matricula=matricula, aves=aves, foto=foto)
             ponto.save()
+
+            title = str(data)
+            html_content = """
+            <h1>Sulgipe</h1>
+            <h2>Registro de Ponto</h2>
+            <hr>
+            <p>Data|Hora: {}</p>
+            <p>Matrícula: {}</p>
+            <hr>            
+            <a href="https://pontosulconta.pythonanywhere.com/visualizacao/{}">
+                Visualize a foto clicando aqui!
+            </a>
+            """.format(data, matricula, ponto.pk)
+
+            text_content = title
+            from_mail = settings.EMAIL_HOST_USER
+            msg = EmailMultiAlternatives(
+                title, 
+                text_content, 
+                from_mail, 
+                ['nathanbabahia@gmail.com', 'junioroliveiraeng@icloud.com', ]
+            )
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
 
             messages.success(request, "Foto salva com sucesso.")
             form = PontoForm()
@@ -28,7 +56,14 @@ def index(request):
     return render(request, 'index.html', context)
 
 
+@login_required
 def historico(request):
     pontos = Ponto.objects.all().order_by("-pk")[:7]
     context = {'pontos': pontos}
     return render(request, 'historico.html', context)
+
+
+@login_required
+def visualizar_foto(request, pk):
+    foto = Ponto.objects.get(pk=pk)
+    return render(request, 'visualizacao.html', {'foto': foto})

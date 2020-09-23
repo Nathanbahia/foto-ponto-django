3303862ede6619d2327c3bfd5e2e373c6091dc2f
nathanbahia <nathanbabahia@gmail.com>
2020-09-22 21:01:22 -0300

    Altera obrigação de fotos

--- ponto/models.py
+++ ponto/models.py
@@ -9,4 +9,4 @@ class Ponto(models.Model):
     data = models.CharField("Data", max_length=30)
     matricula = models.CharField("Matrícula", max_length=10)
     aves = models.CharField('Presença de aves no local?', choices=AVES, max_length=4)
-    foto = StdImageField("", upload_to='media')
+    foto = StdImageField("", upload_to='media', blank=True, null=True)    
